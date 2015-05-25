92270afe2f4d9d375d530d44ec6c363f00a5e22d
greenandlonely <jclarke0206@gmail.com>
2015-05-25 16:02:20 +0100

    something or other

--- _site/js/js.js
+++ _site/js/js.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
+
+$(document).ready(function(){
+	var prevPage = $('#prev');
+	var nextPage = $('#next');
+	
+	$(document).on('keydown', function (event) {
+	    if (event.which == 37) {
+			if(prevPage.length){
+	        	window.location = prevPage.attr('href');
+	        }
+	    } else if (event.which == 39) {
+			if(nextPage.length){
+		        window.location = nextPage.attr('href');
+		    }
+	    }
+	});
+	
+	
 	$('.blogroll > li').shuffle();	
 });
